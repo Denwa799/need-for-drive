d71b3c33b7fdb792447af9e520c297f1e2572d91
Denwa <denwa799@yandex.ru>
2022-03-14 20:07:21 +0300

    Первый коммит

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.less';
+import styles from './App.module.less';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Button type="primary">Работает</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+declare module '*.css';
+declare module '*.less';
+declare module '*.png';
+declare module '*.jpg';
