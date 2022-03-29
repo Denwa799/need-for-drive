91e4aff50480e3106c0a5ff763046a9de7debcf8
Denwa799 <denwa799@yandex.ru>
2022-03-29 21:51:41 +0300

    Сделал фильтрацию машин

---
+++ src/components/pages/Order/FormModel/CarMock.ts
@@ -0,0 +1,110 @@
+export const CarMock = [
+  {
+    id: '1',
+    name: 'ELENTRA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Эконом',
+    },
+  },
+  {
+    id: '2',
+    name: 'I30 N',
+    priceMin: 10000,
+    priceMax: 32000,
+    categoryId: {
+      name: 'Премиум',
+    },
+  },
+  {
+    id: '3',
+    name: 'CRETA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Эконом',
+    },
+  },
+  {
+    id: '4',
+    name: 'SONATA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Премиум',
+    },
+  },
+  {
+    id: '5',
+    name: 'ELENTRA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Эконом',
+    },
+  },
+  {
+    id: '6',
+    name: 'I30 N',
+    priceMin: 10000,
+    priceMax: 32000,
+    categoryId: {
+      name: 'Премиум',
+    },
+  },
+  {
+    id: '7',
+    name: 'CRETA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Эконом',
+    },
+  },
+  {
+    id: '8',
+    name: 'SONATA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Премиум',
+    },
+  },
+  {
+    id: '9',
+    name: 'ELENTRA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Эконом',
+    },
+  },
+  {
+    id: '10',
+    name: 'I30 N',
+    priceMin: 10000,
+    priceMax: 32000,
+    categoryId: {
+      name: 'Премиум',
+    },
+  },
+  {
+    id: '11',
+    name: 'CRETA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Эконом',
+    },
+  },
+  {
+    id: '12',
+    name: 'SONATA',
+    priceMin: 12000,
+    priceMax: 25000,
+    categoryId: {
+      name: 'Премиум',
+    },
+  },
+];
--- src/components/pages/Order/FormModel/FormModel.tsx
+++ src/components/pages/Order/FormModel/FormModel.tsx
@@ -4,40 +4,53 @@ import cn from 'classnames';
 import styles from './FormModel.module.less';
 
 import car1 from '../../../../assets/img/cars/image-1.webp';
-import car2 from '../../../../assets/img/cars/image-2.webp';
-import car3 from '../../../../assets/img/cars/image-3.webp';
-import car4 from '../../../../assets/img/cars/image-4.webp';
+import { CarMock } from './CarMock';
 
 const { Title, Text } = Typography;
 
 const FormModel: FC = () => {
-  const [value, setValue] = useState('all');
+  // Фейковый массив объектов с машинами
+  const cars = CarMock;
 
+  // Стейт для фильтрации в radio button
+  const [filterValue, setFilterValue] = useState('Все');
+
+  // Обработчик нажатия на radio button
   const onChangeHandler = (e: any) => {
     // Стоит any, так как сюда приходит событие и value из компоненты radio библиотеки ant design
-    setValue(e.target.value);
+    setFilterValue(e.target.value);
   };
+
+  // Отфильтровываю машины
+  let filteredCars;
+  if (filterValue !== 'Все') {
+    filteredCars = cars.filter((car) => {
+      return car.categoryId.name === filterValue;
+    });
+  } else {
+    filteredCars = cars;
+  }
 
   return (
     <div className={styles.FormModel}>
       <Affix offsetTop={135}>
         <div className={styles.radioBtnsAffix}>
-          <Radio.Group onChange={onChangeHandler} value={value} className={styles.RadioGroup}>
+          <Radio.Group onChange={onChangeHandler} value={filterValue} className={styles.RadioGroup}>
             <Radio
-              value="all"
-              className={cn(styles.Radio, { [styles.RadioActive]: value === 'all' })}
+              value="Все"
+              className={cn(styles.Radio, { [styles.RadioActive]: filterValue === 'Все' })}
             >
               Все модели
             </Radio>
             <Radio
-              value="econom"
-              className={cn(styles.Radio, { [styles.RadioActive]: value === 'econom' })}
+              value="Эконом"
+              className={cn(styles.Radio, { [styles.RadioActive]: filterValue === 'Эконом' })}
             >
               Эконом
             </Radio>
             <Radio
-              value="prem"
-              className={cn(styles.Radio, { [styles.RadioActive]: value === 'prem' })}
+              value="Премиум"
+              className={cn(styles.Radio, { [styles.RadioActive]: filterValue === 'Премиум' })}
             >
               Премиум
             </Radio>
@@ -46,50 +59,23 @@ const FormModel: FC = () => {
       </Affix>
 
       <Row className={styles.cards}>
-        <Col xl={12} lg={24} md={12} sm={12} xs={24} className={styles.card}>
-          <div className={styles.cardTitleContainer}>
-            <Title level={5} className={styles.cardTitle}>
-              ELANTRA
-            </Title>
-            <Text className={styles.cardText}>12 000 - 25 000 ₽</Text>
-          </div>
-          <div className={styles.imgContainer}>
-            <img src={car1} alt="car" className={styles.img} />
-          </div>
-        </Col>
-        <Col xl={12} lg={24} md={12} sm={12} xs={24} className={styles.card}>
-          <div className={styles.cardTitleContainer}>
-            <Title level={5} className={styles.cardTitle}>
-              I30 N
-            </Title>
-            <Text className={styles.cardText}>10 000 - 32 000 ₽</Text>
-          </div>
-          <div className={styles.imgContainer}>
-            <img src={car2} alt="car" className={styles.img} />
-          </div>
-        </Col>
-        <Col xl={12} lg={24} md={12} sm={12} xs={24} className={styles.card}>
-          <div className={styles.cardTitleContainer}>
-            <Title level={5} className={styles.cardTitle}>
-              CRETA
-            </Title>
-            <Text className={styles.cardText}>12 000 - 25 000 ₽</Text>
-          </div>
-          <div className={styles.imgContainer}>
-            <img src={car3} alt="car" className={styles.img} />
-          </div>
-        </Col>
-        <Col xl={12} lg={24} md={12} sm={12} xs={24} className={styles.card}>
-          <div className={styles.cardTitleContainer}>
-            <Title level={5} className={styles.cardTitle}>
-              SONATA
-            </Title>
-            <Text className={styles.cardText}>10 000 - 32 000 ₽</Text>
-          </div>
-          <div className={styles.imgContainer}>
-            <img src={car4} alt="car" className={styles.img} />
-          </div>
-        </Col>
+        {filteredCars.map((car) => {
+          return (
+            <Col xl={12} lg={24} md={12} sm={12} xs={24} className={styles.card}>
+              <div className={styles.cardTitleContainer}>
+                <Title level={5} className={styles.cardTitle}>
+                  {car.name}
+                </Title>
+                <Text className={styles.cardText}>
+                  {car.priceMin} - {car.priceMax} ₽
+                </Text>
+              </div>
+              <div className={styles.imgContainer}>
+                <img src={car1} alt="car" className={styles.img} />
+              </div>
+            </Col>
+          );
+        })}
       </Row>
     </div>
   );
