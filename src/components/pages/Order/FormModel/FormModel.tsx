67914318f3f50e5a4e8b00f17072ec08d87e4baf
Denwa799 <denwa799@yandex.ru>
2022-03-29 19:37:03 +0300

    Добавил radio button

--- src/components/pages/Order/FormModel/FormModel.tsx
+++ src/components/pages/Order/FormModel/FormModel.tsx
@@ -1,11 +1,36 @@
-import React, { FC } from 'react';
-import AppContainer from '../../../ui/AppLayout/AppContainer/AppContainer';
+import React, { FC, useState } from 'react';
+import { Radio } from 'antd';
+import cn from 'classnames';
+import styles from './FormModel.module.less';
 
 const FormModel: FC = () => {
+  const [value, setValue] = useState('all');
+
+  const onChangeHandler = (e: any) => {
+    // Стоит any, так как сюда приходит событие и value из компоненты radio библиотеки ant design
+    setValue(e.target.value);
+  };
+
   return (
-    <AppContainer>
-      <h2>Сверстать форму оформления заказа на аренду авто (шаг 2)</h2>
-    </AppContainer>
+    <div className={styles.FormModel}>
+      <Radio.Group onChange={onChangeHandler} value={value} className={styles.RadioGroup}>
+        <Radio value="all" className={cn(styles.Radio, { [styles.RadioActive]: value === 'all' })}>
+          Все модели
+        </Radio>
+        <Radio
+          value="econom"
+          className={cn(styles.Radio, { [styles.RadioActive]: value === 'econom' })}
+        >
+          Эконом
+        </Radio>
+        <Radio
+          value="prem"
+          className={cn(styles.Radio, { [styles.RadioActive]: value === 'prem' })}
+        >
+          Премиум
+        </Radio>
+      </Radio.Group>
+    </div>
   );
 };
 
