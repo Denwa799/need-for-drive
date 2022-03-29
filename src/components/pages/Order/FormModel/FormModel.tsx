cb0c8979827588f19cd87cca0de804d19f2e66c6
Denwa799 <denwa799@yandex.ru>
2022-03-29 18:35:09 +0300

    Сверстать форму оформления заказа на аренду авто (шаг 2)

--- src/components/pages/Order/FormModel/FormModel.tsx
+++ src/components/pages/Order/FormModel/FormModel.tsx
@@ -4,7 +4,7 @@ import AppContainer from '../../../ui/AppLayout/AppContainer/AppContainer';
 const FormModel: FC = () => {
   return (
     <AppContainer>
-      <h2>Форма выбора модели машины</h2>
+      <h2>Сверстать форму оформления заказа на аренду авто (шаг 2)</h2>
     </AppContainer>
   );
 };
