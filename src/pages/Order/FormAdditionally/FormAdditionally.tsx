cd4570e19805a827ffe537a6897b6bdb5d1a2a86
Denwa799 <denwa799@yandex.ru>
2022-04-08 10:49:41 +0300

    V-04 Сверстать форму оформления заказа на аренду авто (шаг 3)

--- src/pages/Order/FormAdditionally/FormAdditionally.tsx
+++ src/pages/Order/FormAdditionally/FormAdditionally.tsx
@@ -4,7 +4,7 @@ import React, { FC } from 'react';
 const FormAdditionally: FC = () => {
   return (
     <AppContainer>
-      <h2>Форма выбора дополнительных параметров</h2>
+      <h2>Сверстать форму оформления заказа на аренду авто (шаг 3)</h2>
     </AppContainer>
   );
 };
