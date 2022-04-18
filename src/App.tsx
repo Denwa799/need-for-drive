021f5935a5fd454671dfef7c2eba1153f5ee83d3
Denwa799 <denwa799@yandex.ru>
2022-04-18 14:49:39 +0300

    V-04 Переводит календарь на русский язык

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import 'antd/dist/antd.less';
-import locale from 'antd/lib/locale/ru_RU';
-import { ConfigProvider } from 'antd';
 import AppRouter from './components/AppRouter/AppRouter';
 
 const App = () => {
-  return (
-    <ConfigProvider locale={locale}>
-      <AppRouter />
-    </ConfigProvider>
-  );
+  return <AppRouter />;
 };
 
 export default App;
